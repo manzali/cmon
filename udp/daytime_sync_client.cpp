67f76460d5d578f86151fc21ced6a04c2f84bf7d
Matteo Manzalli <matteo.manzali@gmail.com>
2016-03-10 16:42:26 +0100

    created client-server udp.

--- udp/daytime_sync_client.cpp
+++ udp/daytime_sync_client.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <boost/array.hpp>
 #include <boost/asio.hpp>
 
 using boost::asio::ip::udp;
@@ -19,11 +20,15 @@ int main(int argc, char* argv[]) {
     udp::socket socket(io_service);
     socket.open(udp::v4());
 
-    for (int i = 0; i < 10000; ++i) {
-      std::string str = std::to_string(i);
-      socket.send_to(boost::asio::buffer(str), receiver_endpoint);
-    }
+    boost::array<char, 1> send_buf = { { 0 } };
+    socket.send_to(boost::asio::buffer(send_buf), receiver_endpoint);
 
+    boost::array<char, 128> recv_buf;
+    udp::endpoint sender_endpoint;
+    size_t len = socket.receive_from(boost::asio::buffer(recv_buf),
+                                     sender_endpoint);
+
+    std::cout.write(recv_buf.data(), len);
   } catch (std::exception& e) {
     std::cerr << e.what() << std::endl;
   }
